8a46ec4e8cdeff72be78c1176ff4ca773f3d4163
king001-cooder <sailasjohns17@gmail.com>
2022-03-12 01:40:12 -0800

    Alphabets, lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Program entry p
+ *
+ * Return: 0 on success.
+ */
 int main(void)
 {
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-  /* your code goes there */
 char a = 'a';
-int n =  0;
+int n = 0;
 while (n < 26)
 {
 putchar(a);
